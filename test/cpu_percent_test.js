554e20f3139e81c9e5a90c6ca77e019e9be12421
Christian Amor Kvalheim <christkv@gmail.com>
2012-09-06 09:54:21 +0200

    Added linux cpu_percent functionality

--- test/cpu_percent_test.js
+++ test/cpu_percent_test.js
@@ -21,3 +21,39 @@ exports['Should correctly fetch cpu usage percentages'] = function(test) {
     });
   });
 }
+
+exports['Should correctly fetch cpu usage percentages on linux'] = function(test) {
+  var psUtil = new PSUtil();
+  // Save the existing setup
+  var platform = process.platform;
+  var lib_function = psUtil._lib.cpu_times;
+
+  // Setup Overrides
+  process.platform = 'linux'
+  psUtil.cpu_times = function(perCpu, callback) {
+    if(!perCpu) {
+      callback(null, { user: 21371137, nice: 0, system: 10084998, idle: 177731694 });
+    } else {
+      callback(null, [
+        { user: 6510936, nice: 0, system: 5011204, idle: 40775388 },
+        { user: 3728987, nice: 0, system: 1011353, idle: 47556441 },
+        { user: 7249526, nice: 0, system: 3078746, idle: 41968513 },
+        { user: 3881692, nice: 0, system: 983696, idle: 47431390 } ]);
+    }
+  }
+
+  psUtil.cpu_percent(0.1, false, function(err, result) {
+    test.equal(null, err);
+    test.equal(0, result);
+
+    // Reset platform changes and finish test
+    psUtil.cpu_percent(0.1, true, function(err, result) {
+      test.equal(null, err);
+      test.deepEqual([0, 0, 0, 0], result);
+
+      process.platform = platform;
+      psUtil.cpu_times = lib_function;
+      test.done();
+    });
+ });
+}
--- test/cpu_times_test.js
+++ test/cpu_times_test.js
@@ -1,4 +1,5 @@
-var PSUtil = require('../lib/psutil').PSUtil;
+var PSUtil = require('../lib/psutil').PSUtil,
+  fs = require('fs');
 
 exports.setUp = function(callback) {
   callback();
@@ -20,4 +21,50 @@ exports['Should correctly fetch cpu times'] = function(test) {
       test.done();
     });
   });
+}
+
+exports['Should correctly fetch cpu times on linux'] = function(test) {
+  var psUtil = new PSUtil();
+  // Save the existing setup
+  var platform = process.platform;
+  var readFileFunction = PSUtil._readFile;
+  var lib_function = psUtil._lib.sys_conf;
+
+  // Setup Overrides
+  process.platform = 'linux'
+  PSUtil._readFile = function(path, encoding, callback) {
+    fs.readFile('./test/linux/stat.txt', encoding, callback);
+  }
+  psUtil._lib.sys_conf = function(code, callback) {
+    callback(null, 100);
+  }
+
+  psUtil.cpu_times(false, function(err, result) {
+    test.equal(null, err);
+    test.equal('number', typeof result.user);
+    test.equal('number', typeof result.nice);
+    test.equal('number', typeof result.system);
+    test.equal('number', typeof result.idle);
+    test.equal('number', typeof result.iowait);
+    test.equal('number', typeof result.irq);
+    test.equal('number', typeof result.softirq);
+
+    // Reset platform changes and finish test
+    psUtil.cpu_times(true, function(err, result) {
+      result = result[0];
+      test.equal(null, err);
+      test.equal('number', typeof result.user);
+      test.equal('number', typeof result.nice);
+      test.equal('number', typeof result.system);
+      test.equal('number', typeof result.idle);
+      test.equal('number', typeof result.iowait);
+      test.equal('number', typeof result.irq);
+      test.equal('number', typeof result.softirq);
+
+      process.platform = platform;
+      PSUtil._readFile = readFileFunction;
+      psUtil._lib.sys_conf = lib_function;
+      test.done();
+    });
+ });
 }
